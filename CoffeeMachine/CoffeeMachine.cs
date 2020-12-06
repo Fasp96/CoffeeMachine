ba81bd728d4bf64cd626529db89369ce22e0ab75
Fábio Pacheco <38566316+Fasp96@users.noreply.github.com>
2020-12-06 00:10:39 +0000

    Interactive Console CofeeMachine

--- CoffeeMachine/CoffeeMachine.cs
+++ CoffeeMachine/CoffeeMachine.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using CoffeeMachine.Data;
 
 namespace CoffeeMachine
@@ -7,36 +8,111 @@ namespace CoffeeMachine
     {
         static void Main(string[] args)
         {
-            //Using CreateExpresso and AddMocha
+            string coffeeSelected;
+            string[] condimentsSelected;
             CoffeFactory factory = CoffeFactory.Instance;
-            ICoffee coffee = factory.CreateEspresso();
-            Console.WriteLine(coffee.Description + "\n  Price: " + coffee.Cost + " euros\n");
+            ICoffee coffee;
 
+            ShowCoffeeOpt(); //Show Coffee Options
+            coffeeSelected = ReadCoffeeOpt(); //Reads Coffee Option
+            
+            ShowCoffeeSelected(coffeeSelected); //Show selected coffe and if wants condiments
+            coffee = factory.CreateCoffee(coffeeSelected); //Creates the selected coffe
+            
+            if (Console.ReadKey(true).Key == ConsoleKey.Y)
+            {
+                ShowCondimentOpt(); //Show Condiment Options if Y is pressed
+                condimentsSelected = ReadCondimentsOpt(); //Read Coffee Option
+                foreach (string condiment in condimentsSelected) coffee = factory.AddCondiment(coffee, condiment); //Creates the coffe with the selected condiments
+            }
 
-            coffee = factory.AddMocha(coffee);
-            Console.WriteLine(coffee.Description + "\n  Price: " + coffee.Cost + " euros\n");
+            ShowCoffeeCreated(coffee); //Shows the created Coffe
+            Console.Read();
+        }
 
-            // Using CreateDecaf and AddSteamedMilk
-            ICoffee coffee1 = factory.CreateDecaf();
-            Console.WriteLine(coffee1.Description + "\n  Price: " + coffee1.Cost + " euros\n");
+        static void ShowCoffeeOpt()
+        {
+            Console.Write(
+                "\t** Wellcome to the Coffe Machine **\n" + 
+                "\n" +
+                "Coffe Options:\n" +
+                "\t(1) House Blend\t 0.89 euros\n" +
+                "\t(2) Dark Roast\t 0.99 euros\n" +
+                "\t(3) Decaf\t 1.05 euros\n" +
+                "\t(4) Espresso\t 1.99 euros\n" +
+                "\t(5) Green Tea\t 1.00 euros\n" +
+                "\n" +
+                "Please select your desire coffee or tea: "
+                );
+        }
 
-            coffee1 = factory.AddSteamedMilk(coffee1);
-            Console.WriteLine(coffee1.Description + "\n  Price: " + coffee1.Cost + " euros\n");
+        static void ShowCoffeeSelected(string coffeeSelected)
+        {
+            Console.Write("\nThe selected coffe was {0}, would you like some condiments in you coffee?(Press Y for condiments)", coffeeSelected);
+        }
 
-            // Using Createcoffee and AddCondiment
-            ICoffee coffee2 = factory.CreateCoffee("House Blend");
-            Console.WriteLine(coffee2.Description + "\n  Price: " + coffee2.Cost + " euros\n");
+        static string ReadCoffeeOpt()
+        {
+            string coffeeOpt = Console.ReadLine();
 
-            coffee2 = factory.AddCondiment(coffee2, "Whip");
-            Console.WriteLine(coffee2.Description + "\n  Price: " + coffee2.Cost + " euros\n");
+            switch (coffeeOpt)
+            {
+                case "1": return "House Blend";
+                case "2": return "Dark Roast";
+                case "3": return "Decaf";
+                case "4": return "Espresso";
+                case "5": return "Green Tea";
+            }
+            throw new Exception();
+        }
 
-            // Using CreateGreenTea
-            ICoffee tea = factory.CreateGreenTea();
-            Console.WriteLine(tea.Description + "\n  Price: " + tea.Cost + " euros\n");
+        static void ShowCondimentOpt()
+        {
+            Console.Write(
+                "\n\nCondiment Options:\n" +
+                "\t(1) Steamed Milk 0.10 euros\n" +
+                "\t(2) Mocha\t 0.20 euros\n" +
+                "\t(3) Soy\t\t 0.15 euros\n" +
+                "\t(4) Whip\t 0.10 euros\n" +
+                "\n" +
+                "Please select your desire coffee or tea: "
+                );
+        }
 
-            // Using CreateCoffee to create Tea
-            ICoffee tea1 = factory.CreateGreenTea();
-            Console.WriteLine(tea1.Description + "\n  Price: " + tea1.Cost + " euros\n");
+        static string[] ReadCondimentsOpt()
+        {
+            string condimentsOpt;
+            string[] condimentsOptArray;
+
+            condimentsOpt = Console.ReadLine().Replace(" ", string.Empty);
+            condimentsOptArray = condimentsOpt.Split(',');
+
+            for (int i = 0; i < condimentsOptArray.Length; i++)
+            {
+                switch (condimentsOptArray[i])
+                {
+                    case "1": condimentsOptArray[i] = "Steamed Milk";
+                        break;
+                    case "2": condimentsOptArray[i] = "Mocha";
+                        break;
+                    case "3": condimentsOptArray[i] = "Soy";
+                        break;
+                    case "4": condimentsOptArray[i] = "Whip";
+                        break;
+                    default: throw new Exception();
+                }
+                
+            }
+            return condimentsOptArray;
+        }
+
+        static void ShowCoffeeCreated(ICoffee coffee)
+        {
+            Console.Write(
+                "\n"+
+                "\nCoffee Created: {0}" +
+                "\nCost: {1} euros"
+                , coffee.Description, Math.Round(coffee.Cost, 2));
         }
     }
 }
